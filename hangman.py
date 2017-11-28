fce93ce5975f79e67a88e6e38e6b7c94fb55ec00
jhat3 <31477572+jhat3@users.noreply.github.com>
2017-11-28 09:31:12 -0500

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,5 +1,34 @@
+#Hangman
+#Justin Hatley
+
+def start_screen():
+    print("""
+ ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄       ▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄ 
+▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░░▌     ▐░░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌
+▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌░▌   ▐░▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌░▌     ▐░▌
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌▐░▌    ▐░▌▐░▌          ▐░▌▐░▌ ▐░▌▐░▌▐░▌       ▐░▌▐░▌▐░▌    ▐░▌
+▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌ ▐░▌   ▐░▌▐░▌ ▄▄▄▄▄▄▄▄ ▐░▌ ▐░▐░▌ ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌ ▐░▌   ▐░▌
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌▐░▌▐░░░░░░░░▌▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌
+▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌   ▐░▌ ▐░▌▐░▌ ▀▀▀▀▀▀█░▌▐░▌   ▀   ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌   ▐░▌ ▐░▌
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌    ▐░▌▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌    ▐░▌▐░▌
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌     ▐░▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌     ▐░▐░▌
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌      ▐░░▌
+ ▀         ▀  ▀         ▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀         ▀  ▀        ▀▀ 
+    """)
+    
+def show_credits():
+    print()
+    print("""
+███╗   ███╗ █████╗ ██████╗ ███████╗    ██████╗ ██╗   ██╗         ██╗██╗   ██╗███████╗████████╗██╗███╗   ██╗    ██╗  ██╗ █████╗ ████████╗██╗     ███████╗██╗   ██╗
+████╗ ████║██╔══██╗██╔══██╗██╔════╝    ██╔══██╗╚██╗ ██╔╝         ██║██║   ██║██╔════╝╚══██╔══╝██║████╗  ██║    ██║  ██║██╔══██╗╚══██╔══╝██║     ██╔════╝╚██╗ ██╔╝
+██╔████╔██║███████║██║  ██║█████╗      ██████╔╝ ╚████╔╝          ██║██║   ██║███████╗   ██║   ██║██╔██╗ ██║    ███████║███████║   ██║   ██║     █████╗   ╚████╔╝ 
+██║╚██╔╝██║██╔══██║██║  ██║██╔══╝      ██╔══██╗  ╚██╔╝      ██   ██║██║   ██║╚════██║   ██║   ██║██║╚██╗██║    ██╔══██║██╔══██║   ██║   ██║     ██╔══╝    ╚██╔╝  
+██║ ╚═╝ ██║██║  ██║██████╔╝███████╗    ██████╔╝   ██║       ╚█████╔╝╚██████╔╝███████║   ██║   ██║██║ ╚████║    ██║  ██║██║  ██║   ██║   ███████╗███████╗   ██║   
+╚═╝     ╚═╝╚═╝  ╚═╝╚═════╝ ╚══════╝    ╚═════╝    ╚═╝        ╚════╝  ╚═════╝ ╚══════╝   ╚═╝   ╚═╝╚═╝  ╚═══╝    ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚══════╝   ╚═╝                                                                                                                                                               
+    """)
+    
 def get_puzzle():
-    pass
+    return "yessir"
 
 def get_solved(puzzle, guesses):
     solved = ""
@@ -13,15 +42,80 @@ def get_solved(puzzle, guesses):
     return solved
 
 def get_guess():
-    pass
+    while True:
+        letter = input("Guess a letter: ")
+
+        if len(letter) == 1:
+            if(letter).isalpha():
+                return letter
+            else:
+                print("Enter one letter at a time.")
+        else:
+                print("Enter one letter at a time.")    
 
 def display_board(solved):
-    pass
+    print(solved)
 
-def show_result():
-    pass
+def show_result(result):
+    if result == 0:
+         print("Congratulations!")
+    else:
+        print("Loser.")
     
-def play():
-    pass
+
+def play_again(name):
+    while True:
+        decision = input("Do you wanna play again " + name + " ? (y/n)")
+        print()
+        decision = decision.lower()
+
+        if decision == "y" or decision == "yes":
+            return True
+        elif decision == "n" or decision == "no":
+            print("Goodbye!")
+            return False
+        
+        else:
+            print("Please enter a valid answer.")
     
-play()
+def play(name):
+    print("Welcome to Hangman Game, " + name + "!!")
+    print("I will give you 5 guesses to guess the word.")
+    puzzle = get_puzzle()
+    guesses = ""
+    solved = get_solved(puzzle, guesses)
+    display_board(solved)
+
+    strikes = 0
+    limit = 5
+    result = 0
+    print(solved)
+    gameover = 0
+
+    while solved != puzzle and gameover == 0:
+        letter = get_guess()
+
+        if letter not in puzzle:
+            strikes +=1
+            print("That letter isn't in the word!")
+            print("You have " + str(strikes) + " strikes. Try not to lose " + name + " .")
+            print()
+            if strikes == limit:
+                print("You lost " + name + ", that word was easy too!")
+                result = 1
+                gameover = 1
+        
+        guesses += letter
+        solved = get_solved(puzzle, guesses)
+        display_board(solved)
+
+    show_result(result)
+
+start_screen()
+playing = True
+
+while playing:
+    name = input("What is your name?")
+    play(name)
+    playing = play_again(name)
+show_credits()
